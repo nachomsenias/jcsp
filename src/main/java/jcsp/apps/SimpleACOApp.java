9c2e4a714fef4edb7dda0a8d3addd01e222a1111
ignacio.moya <ignacio.moya@softcomputing.es>
2015-11-19 17:17:22 +0100

    Added new configurations for hibridations ACO+LS.

--- src/main/java/jcsp/apps/SimpleACOApp.java
+++ src/main/java/jcsp/apps/SimpleACOApp.java
@@ -1,13 +1,22 @@
 package jcsp.apps;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import jcsp.CSPProblem;
+import jcsp.CSPSolution;
 import jcsp.algo.ACO;
 import jcsp.experiment.beans.ACOBean;
+import jcsp.localsearch.FirstImprovement;
+import jcsp.localsearch.LocalSearch;
+import jcsp.neighbourhood.CSPSwapNeighbourhood;
 import jcsp.util.CSPParser;
+
+import org.jamesframework.core.search.neigh.Neighbourhood;
+
 import util.random.RandomizerFactory;
 import util.random.RandomizerFactory.RandomizerAlgorithm;
 import util.random.RandomizerUtils;
@@ -18,8 +27,8 @@ public class SimpleACOApp {
 //		String exampleFile="../xCSP/instances/test_10_cars.txt";
 //		String exampleFile="../xCSP/instances/60/pb_60-05.txt";
 //		String exampleFile="../xCSP/instances/90/pb_90-05.txt";
-		String exampleFile="../xCSP/instances/classical/p41_66.txt";
-//		String exampleFile="./instances/200/pb_200_01.txt";
+//		String exampleFile="../xCSP/instances/classical/p41_66.txt";
+		String exampleFile="./instances/200/pb_200_01.txt";
 
 		CSPProblem csp = CSPParser.load(exampleFile);
 		boolean verbose = false;
@@ -46,21 +55,22 @@ public class SimpleACOApp {
 		
 //		long maxSteps = 2000;
 		
-//		List<Neighbourhood<CSPSolution>> localNeighbourhood = new ArrayList<Neighbourhood<CSPSolution>>();
-//		localNeighbourhood.add(new CSPSwapNeighbourhood());
+		List<Neighbourhood<CSPSolution>> localNeighbourhood = new ArrayList<Neighbourhood<CSPSolution>>();
+		localNeighbourhood.add(new CSPSwapNeighbourhood());
 //		localNeighbourhood.add(new CSPInvertionNeighbourhood());
 		
-//		LocalSearch localSearch = new FirstImprovement(localNeighbourhood);
+		LocalSearch localSearch = new FirstImprovement(localNeighbourhood);
 		
-//		List<Neighbourhood<CSPSolution>> globalNeighbourhood = new ArrayList<Neighbourhood<CSPSolution>>();
+		List<Neighbourhood<CSPSolution>> globalNeighbourhood = new ArrayList<Neighbourhood<CSPSolution>>();
 //		globalNeighbourhood.add(new CSPInvertionNeighbourhood());
+		globalNeighbourhood.add(new CSPSwapNeighbourhood());
 		
-//		LocalSearch overAllSearch = new FirstImprovement(globalNeighbourhood);
+		LocalSearch overAllSearch = new FirstImprovement(globalNeighbourhood);
 		
 		System.out.println("Starting ACO experiment with file: " + exampleFile);
 		
 		final ACOBean bean = new ACOBean(ants, maxCycles, alpha, beta, delta, 
-				q0, tau0, localRho, globalRho, null, null);
+				q0, tau0, localRho, globalRho, localSearch, overAllSearch);
 
 		for (int seedIndex = 0; seedIndex<30; seedIndex++) { 
 	        System.out.println(new Date().toString());
